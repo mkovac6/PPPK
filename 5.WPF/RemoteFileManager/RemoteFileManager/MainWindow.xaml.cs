064d8f38e0670203289dc4f774c37090ccc7c47c
mkovac6 <mkovac6@racunarstvo.hr>
2023-09-07 13:58:29 +0200

    Dodano sve do testiranja funkcionalnosti, treba dodati file-ove na server.

--- 5.WPF/RemoteFileManager/RemoteFileManager/MainWindow.xaml.cs
+++ 5.WPF/RemoteFileManager/RemoteFileManager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RemoteFileManager.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,43 @@ namespace RemoteFileManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ItemsViewModel itemsViewModel;
         public MainWindow()
         {
             InitializeComponent();
+            itemsViewModel = new ItemsViewModel();
+            Init();
+        }
+
+        private void Init()
+        {
+            CbDirectories.ItemsSource = itemsViewModel.Directories;
+            LbItems.ItemsSource = itemsViewModel.Items;
+        }
+
+        private void CbDirectories_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void CbDirectories_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LbItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnUpload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnUpload_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- 5.WPF/RemoteFileManager/RemoteFileManager/Properties/AssemblyInfo.cs
+++ 5.WPF/RemoteFileManager/RemoteFileManager/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RemoteFileManager")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
