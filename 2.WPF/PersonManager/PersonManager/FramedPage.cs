5594855e12fd7a2e85263fd1cb7855cad2786990
mkovac6 <mkovac6@racunarstvo.hr>
2023-09-11 15:10:28 +0200

    Dodan projekt 2. WPF(PersonManager)

---
+++ 2.WPF/PersonManager/PersonManager/FramedPage.cs
@@ -0,0 +1,15 @@
+﻿using PersonManager.ViewModels;
+using System.Windows.Controls;
+
+namespace Zadatak
+{
+    public class FramedPage : Page
+    {
+        public FramedPage(PersonViewModel personViewModel)
+        {
+            PersonViewModel = personViewModel;
+        }
+        public PersonViewModel PersonViewModel { get; }
+        public Frame Frame { get; set; }
+    }
+}
